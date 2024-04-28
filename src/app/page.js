6a0d6335c1ec4bb3227e56de393010a43090e6e8
Anietie Brownson <anni.brownson@gmail.com>
2024-04-28 14:56:33 +0100

    feat: Add desktop view for page 1 and 2

--- src/app/page.js
+++ src/app/page.js
@@ -5,98 +5,139 @@ import Mobile from "../../public/images/bg-sidebar-mobile.svg";
 
 export default function Home() {
   return (
-    <main className="relative md:px-5">
-      <Image src={Desktop} alt="" className="hidden md:block md:h-[100vh]" />
-      <Image src={Mobile} alt="" className="w-full md:hidden" />
+    <main className="relative lg:px-8">
+      <div className="bg-white">
+        <Image src={Desktop} alt="" className="hidden md:block md:h-[100vh]" />
+        <Image src={Mobile} alt="" className="w-full md:hidden" />
 
-      {/* Routing buttons for mobile */}
-      <div className="text-white flex justify-between gap-5 absolute md:hidden top-12 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          1
-        </button>
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          2
-        </button>
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          3
-        </button>
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          4
-        </button>
+        {/* Routing buttons for mobile */}
+        <div className="text-white flex justify-between gap-5 absolute md:hidden top-12 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            1
+          </button>
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            2
+          </button>
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            3
+          </button>
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            4
+          </button>
+        </div>
+
+        {/* Routing buttons for desktop */}
+        <div className="text-white md:flex flex-col gap-5 hidden absolute md:top-14 left-14">
+          <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            1
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 1</h1>
+            <p className="text-sm font-medium">YOUR INFO</p>
+          </span>
+          </div>
+          <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            2
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 2</h1>
+            <p className="text-sm font-medium">SELECT PLAN</p>
+          </span>
+          </div>
+          <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            3
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 3</h1>
+            <p className="text-sm font-medium">ADD-ONS</p>
+          </span>
+          </div>
+          <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            4
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 4</h1>
+            <p className="text-sm font-medium">SUMMARY</p>
+          </span>
+          </div>
+        </div>
+
+        {/* form div */}
+        <section className="px-5 flex justify-center items-center">
+          <form className="px-5 py-7 bg-[#fff] text-left w-[90%] md:w-[60%] rounded-lg absolute md:block md:left-[19rem] md:top-5 md:h-[90vh] top-[58%]">
+            <h1 className="text-[#02295a] text-2xl font-bold">Personal info</h1>
+            <p className="text-[#9699ab]">
+              Please provide your name, email address, and phone number.
+            </p>
+
+            <div className="mt-5">
+              <div className="grid grid-cols-6 gap-4 md:gap-0 col-span-full lg:col-span-3">
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="name" className="text-[#02295a] text-base">
+                    Name
+                  </label>
+                  <input
+                    id="firstname"
+                    type="text"
+                    placeholder="e.g. Stephen King"
+                    className="text-[#9699ab] pl-5 font-medium w-full rounded-md py-2 border border-[#9699ab]"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-3 grid grid-cols-6 gap-4 md:gap-0 col-span-full lg:col-span-3">
+                <div className="col-span-full sm:col-span-3 ">
+                  <label htmlFor="email" className="text-[#02295a] text-base">
+                    Email Address
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="e.g. stephenking@lorem.com"
+                    className="text-[#9699ab] pl-5 font-medium w-full rounded-md py-2 border border-[#9699ab]"
+                  />
+                </div>
+              </div>
+
+              <div className="mt-3 grid grid-cols-6 gap-4 md:gap-0 col-span-full lg:col-span-3">
+                <div className="col-span-full sm:col-span-3">
+                  <label htmlFor="number" className="text-[#02295a] text-base">
+                    Phone Number
+                  </label>
+                  <input
+                    id="number"
+                    type="number"
+                    placeholder="e.g. +1 234 567 890"
+                    className="text-[#9699ab] pl-5 font-medium w-full rounded-md py-2 border border-[#9699ab]"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* next section for desktop view */}
+            <section className="mt-32 items-end justify-end hidden md:flex w-full">
+              <Link href="/select-plan">
+                <button className="bg-[#02295a] py-3 px-5 rounded-md text-white">
+                  Next Step
+                </button>
+              </Link>
+            </section>
+          </form>
+        </section>
+
+        {/* next section */}
+        <section className="px-5 py-4 items-end justify-end bg-white fixed md:hidden bottom-0 flex w-full h-20">
+          <Link href="/select-plan">
+            <button className="bg-[#02295a] py-3 px-5 rounded-md text-white">
+              Next Step
+            </button>
+          </Link>
+        </section>
       </div>
-
-      {/* Routing buttons for desktop */}
-      <div className="text-white md:flex flex-col gap-5 hidden absolute top-20 left-14">
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          1
-        </button>
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          2
-        </button>
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          3
-        </button>
-        <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
-          4
-        </button>
-      </div>
-
-      {/* form div */}
-      <section className="md:hidden px-5 flex justify-center items-center">
-        <form className="px-5 py-7 bg-[#fff] text-left w-[90%] rounded-lg absolute top-[58%]">
-          <h1 className="text-[#02295a] text-2xl font-bold">Personal info</h1>
-          <p className="text-[#9699ab]">
-            Please provide your name, email address, and phone number.
-          </p>
-
-          <div className="mt-5">
-          <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-            <div className="col-span-full sm:col-span-3">
-              <label htmlFor="name" className="text-[#02295a] text-base">
-                Name
-              </label>
-              <input
-                id="firstname"
-                type="text"
-                placeholder="e.g. Stephen King"
-                className="text-[#9699ab] pl-5 font-medium w-full rounded-md py-2 border border-[#9699ab]"
-              />
-            </div>
-          </div>
-
-          <div className="mt-3 grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-            <div className="col-span-full sm:col-span-3">
-            <label htmlFor="email" className="text-[#02295a] text-base">Email Address</label>
-              <input
-                id="email"
-                type="email"
-                placeholder="e.g. stephenking@lorem.com"
-                className="text-[#9699ab] pl-5 font-medium w-full rounded-md py-2 border border-[#9699ab]"
-              />
-            </div>
-          </div>
-
-          <div className="mt-3 grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-            <div className="col-span-full sm:col-span-3">
-              <label htmlFor="number" className="text-[#02295a] text-base">
-              Phone Number
-              </label>
-              <input
-                id="number"
-                type="number"
-                placeholder="e.g. +1 234 567 890"
-                className="text-[#9699ab] pl-5 font-medium w-full rounded-md py-2 border border-[#9699ab]"
-              />
-            </div>
-          </div>
-          </div>
-        </form>
-      </section>
-
-      {/* next section */}
-      <section className="px-5 py-4 items-end justify-end bg-white fixed md:hidden bottom-0 flex w-full h-20">
-        <Link href="/select-plan"><button className="bg-[#02295a] py-3 px-5 rounded-md text-white">Next Step</button></Link>
-      </section>
     </main>
   );
 }
--- src/app/select-plan/page.js
+++ src/app/select-plan/page.js
@@ -12,8 +12,8 @@ export default function SelectPlan() {
       <Image src={Desktop} alt="" className="hidden md:block" />
       <Image src={Mobile} alt="" className="w-full md:hidden" />
 
-      {/* Routing buttons */}
-      <div className="text-white flex justify-between gap-5 absolute top-12 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
+      {/* Routing buttons for mobile */}
+      <div className="text-white flex justify-between gap-5 absolute md:hidden top-12 left-[50%] transform -translate-x-1/2 -translate-y-1/2">
         <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
           1
         </button>
@@ -28,9 +28,49 @@ export default function SelectPlan() {
         </button>
       </div>
 
+      {/* Routing buttons for desktop */}
+      <div className="text-white md:flex flex-col gap-5 hidden absolute md:top-14 left-14">
+        <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            1
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 1</h1>
+            <p className="text-sm font-medium">YOUR INFO</p>
+          </span>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            2
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 2</h1>
+            <p className="text-sm font-medium">SELECT PLAN</p>
+          </span>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            3
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 3</h1>
+            <p className="text-sm font-medium">ADD-ONS</p>
+          </span>
+        </div>
+        <div className="flex items-center gap-4">
+          <button className="flex justify-center items-center p-4 w-3 h-3 border border-white rounded-full">
+            4
+          </button>
+          <span>
+            <h1 className="text-xs text-[#d6d9e6]">STEP 4</h1>
+            <p className="text-sm font-medium">SUMMARY</p>
+          </span>
+        </div>
+      </div>
+
       {/* form div */}
       <section className="px-5 flex justify-center items-center">
-        <form className="px-5 py-7 bg-[#fff] text-left w-[90%] rounded-lg absolute top-[58%]">
+        <form className="px-5 py-7 bg-[#fff] text-left w-[90%] md:w-[60%] rounded-lg absolute md:block md:left-[19rem] md:top-5 md:h-[90vh] top-[58%]">
           <h1 className="text-[#02295a] text-2xl font-bold">
             Select your plan
           </h1>
@@ -38,26 +78,26 @@ export default function SelectPlan() {
             You have the option of monthly or yearly billing.
           </p>
 
-          <div className="mt-5">
-            <div className="rounded-md py-3 pl-4 border border-[#d6d9e6] flex items-center">
+          <div className="mt-5 md:flex flex-row md:gap-5">
+            <div className="rounded-md py-3 pl-4 md:w-full border border-[#d6d9e6] flex md:flex-col items-center md:items-start">
               <Image src={Arcade} alt="" />
-              <div className="pl-3">
+              <div className="pl-3 md:pl-0 md:mt-8">
                 <h1 className="text-[#02295a] text-lg font-bold">Arcade</h1>
                 <p className="text-[#9699ab]">$9/mo</p>
               </div>
             </div>
 
-            <div className="rounded-md mt-3 py-3 pl-4 border border-[#d6d9e6] flex items-center">
+            <div className="rounded-md mt-3 md:mt-0 md:w-full py-3 pl-4 border border-[#d6d9e6] flex md:flex-col items-center md:items-start">
               <Image src={Advanced} alt="" />
-              <div className="pl-3">
+              <div className="pl-3 md:pl-0 md:mt-8">
                 <h1 className="text-[#02295a] text-lg font-bold">Advanced</h1>
                 <p className="text-[#9699ab]">$12/mo</p>
               </div>
             </div>
 
-            <div className="rounded-md mt-3 py-3 pl-4 border border-[#d6d9e6] flex items-center">
+            <div className="rounded-md mt-3 md:mt-0 md:w-full py-3 pl-4 border border-[#d6d9e6] flex md:flex-col items-center md:items-start">
               <Image src={Pro} alt="" />
-              <div className="pl-3">
+              <div className="pl-3 md:pl-0 md:mt-8">
                 <h1 className="text-[#02295a] text-lg font-bold">Pro</h1>
                 <p className="text-[#9699ab]">$15/mo</p>
               </div>
@@ -75,18 +115,32 @@ export default function SelectPlan() {
             />
             <span className="text-[#02295a] text-base font-bold">Yearly</span>
           </section>
+
+          {/* next section */}
+          <section className="mt-32 items-end justify-between hidden md:flex w-full">
+            <Link href="/">
+              <button className="py-3 font-medium text-[#9699ab]">
+                Go Back
+              </button>
+            </Link>
+            <Link href="/select-plan">
+              <button className="bg-[#02295a] py-3 px-5 rounded-md text-white">
+                Next Step
+              </button>
+            </Link>
+          </section>
         </form>
       </section>
 
       {/* next section */}
-      <section className="px-5 py-4 items-end bg-white fixed bottom-0 flex justify-between w-full h-20">
+      <section className="px-5 py-4 items-end bg-white fixed md:hidden bottom-0 flex justify-between w-full h-20">
         <Link href="/">
           <button className="py-3 font-medium text-[#9699ab]">Go Back</button>
         </Link>
         <Link href="/add-ons">
-        <button className="bg-[#02295a] py-3 px-5 rounded-md text-white">
-          Next Step
-        </button>
+          <button className="bg-[#02295a] py-3 px-5 rounded-md text-white">
+            Next Step
+          </button>
         </Link>
       </section>
     </main>
